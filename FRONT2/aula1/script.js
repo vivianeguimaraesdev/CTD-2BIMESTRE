8ac5e31ab8899144e699d545f30201d0a83e370a
Viviane Guimarães <83373229+vivianeguimaraesdev@users.noreply.github.com>
2022-08-01 11:59:39 -0300

    primeira aula 01/08

---
+++ FRONT2/aula1/script.js
@@ -0,0 +1,12 @@
+console.log('Hello World')
+// window.alert('Hello World')   mostra uma janela de alerta com a mensagem interna
+
+let variavel = window.prompt('insira o dado') // mostra uma janela de alerta com a mensagem interna pedindo um input pro usario, atrelado a uma var
+console.log(variavel)
+
+let objetoteste = {
+    nome:'victor',
+    sobrenome:'luz'
+}
+
+document.write(soma) //edita o documento e insere o valor no corpo do HTML
---
+++ FRONT2/aula1/scriptmesatrabalho.js
@@ -0,0 +1,8 @@
+let arrayteste = [1,2,3,4,5]
+let soma = 0
+
+for(var i=0; i < arrayteste.length ; i++)
+{soma = soma +arrayteste[i]
+console.log("Valor da soma : " + soma )// Mostra no console a variavel
+document.write("Valor da soma : "+ soma + "</br>") // Insere o valor no corpo HTML da aplicação
+}
